c634eadaac121b62964c1c57785d45f2ef38e4ee
wwwru <www.rufat@bk.ru>
2023-09-30 22:06:03 +0300

    push_commit_OOP_13

--- src/video.py
+++ src/video.py
@@ -8,22 +8,22 @@ class Video:
     """Класс для ютуб-канала"""
 
     def __init__(self, video_id) -> None:
+        self.video_id = video_id
+        self.title = None
+        self.like_count = None
         try:
 
-            self.video_id = video_id
-            self.title = None
-            self.like_count = None
-        except ValueError:
-            print('нет такого id')
             video = youtube.videos().list(id=self.video_id, part='snippet,statistics').execute()
-            video_data = video['items'][0]
-            self.title = video_data['snippet']['title']
-            self.url = f'https://www.youtube.com/video/{video_id}'
-            self.count_view = video_data['statistics']['viewCount']
-            self.like_count = video_data['statistics']['likeCount']
-
+            if 'items' in video and video['items']:
+                video_data = video['items'][0]
+                self.title = video_data['snippet']['title']
+                self.url = f'https://www.youtube.com/video/{video_id}'
+                self.count_view = video_data['statistics']['viewCount']
+                self.like_count = video_data['statistics']['likeCount']
+        except ValueError:
+            print('нет такого видео')
     def __str__(self):
-        return f'{self.name}'
+        return f'{self.title}'
 
 
 
@@ -33,5 +33,5 @@ class PLVideo(Video):
         self.id_playlist = id_playlist
 
     def __str__(self):
-        return f'{self.name}'
+        return f'{self.title}'
 
