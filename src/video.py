bae498963f36dc1e865681d31be1baf4f08424cb
wwwru <www.rufat@bk.ru>
2023-09-30 21:25:59 +0300

    push_commit_OOP_13

--- src/video.py
+++ src/video.py
@@ -8,13 +8,19 @@ class Video:
     """Класс для ютуб-канала"""
 
     def __init__(self, video_id) -> None:
-        self.video_id = video_id
-        video = youtube.videos().list(id=self.video_id, part='snippet,statistics').execute()
-        video_data = video['items'][0]
-        self.name = video_data['snippet']['title']
-        self.url = f'https://www.youtube.com/video/{video_id}'
-        self.count_view = video_data['statistics']['viewCount']
-        self.like = video_data['statistics']['likeCount']
+        try:
+
+            self.video_id = video_id
+            self.title = None
+            self.like_count = None
+        except ValueError:
+            print('нет такого id')
+            video = youtube.videos().list(id=self.video_id, part='snippet,statistics').execute()
+            video_data = video['items'][0]
+            self.title = video_data['snippet']['title']
+            self.url = f'https://www.youtube.com/video/{video_id}'
+            self.count_view = video_data['statistics']['viewCount']
+            self.like_count = video_data['statistics']['likeCount']
 
     def __str__(self):
         return f'{self.name}'
